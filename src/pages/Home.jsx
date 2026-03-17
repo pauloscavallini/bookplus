4dbc67e3de15f40b0907ac5dbc9d6e77abfc11c3
pauloscavallini <paulo.cavallini08@gmail.com>
2026-03-17 16:47:32 -0300

    feat: adiciona conexao e filtro com banco de dados

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,32 +8,55 @@ import { useState, useEffect } from "react";
 
 export default function Home() {
     const [dados, setDados] = useState([])
+    const [filtro, setFiltro] = useState("")
+
+    async function carregarDados(filtroNome) {
+        let resultadoDados = await fetch(`https://apps-api-livros.ucxocw.easypanel.host/livro?${filtroNome}`, {
+            method: 'GET',
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+        // console.log(await resultadoDados.json())
+        let livros = await resultadoDados.json()
+        return livros.livros;
+    }
 
     useEffect(() => {
-        async function carregarDados() {
-            const resultadoDados = await request("listar_livros", "GET");
-            setDados(resultadoDados && resultadoDados["livros"]);
-            // console.log(resultadoDados["livros"]);
+        const buscar = async () => {
+            var lista1 = await carregarDados("titulo=" + filtro);
+            var lista2 = await carregarDados("autor=" + filtro);
+            var listaFinal = lista1.concat(lista2);
+            setDados(listaFinal);
         }
-        carregarDados();
-    }, [])
+        buscar();
+    }, [filtro])
 
     return (
         <>
         <Banner />
         <div className="container py-5 fw-bold">
             <SectionTitle titulo={"NOVIDADES"} topico={"Destaques da Semana"} />
+            <div className="d-flex">
+                <input
+                className="bg-secondary bg-opacity-25 rounded px-2 py-1"
+                onChange={e => setFiltro(e.target.value)}
+                value={filtro}
+                placeholder="Procure por nome ou autor"
+            />
+            </div>
             <BookGrid>
             {dados ? dados.map((livro, index) => (
                 <BookCard
-                    key={livro.id_livro}
+                    key={index}
                     genero="POESIA"
                     nome={livro["titulo"]}
                     autor={livro["autor"]}
+                    imagem={livro.imagem}
                 />
-            )): dadosLivros.map((livro) => (
+            )): dadosLivros.map((livro, index) => (
                 <BookCard
-                    key={livro.id_livro}
+                    key={index}
                     genero="POESIA"
                     nome={livro["titulo"]}
                     autor={livro["autor"]}
